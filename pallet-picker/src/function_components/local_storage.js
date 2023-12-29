aa6e8659635c9345a8f0d36167bcc5b1a78fba63
MohamedSy1 <mohamed.sy@compscihigh.org>
2023-12-28 23:05:28 -0500

    completed the delete button and the color pallet for the inputed values

---
+++ pallet-picker/src/function_components/local_storage.js
@@ -0,0 +1,34 @@
+import palletData from "../default-pallet.json"
+
+console.log([...palletData])
+// Generic Helper Functions
+export const setLocalStorageKey = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+  
+  export const getLocalStorageKey = (key) => {
+    try {
+      return JSON.parse(localStorage.getItem(key))
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+  
+  // pallet-specific helper functions
+  export const setPallets = (pallets) => setLocalStorageKey('pallets', pallets);
+  export const getPallets = () => getLocalStorageKey('pallets');
+  
+  // modifying pallet functions
+  export const initializePallets = () => setPallets(palletData);
+  
+  export const addPallets = (pallet) => {
+    const pallets = getPallets();
+    setPallets([pallet, ...pallets]);
+  }
+  
+  export const removePallet = (palletToRemove) => {
+    const pallets = getPallets();
+    const filteredPallet = pallets.filter((pallet) => pallet.uuid !== palletToRemove.uuid);
+    setPallets(filteredPallet);
+  }
--- pallet-picker/src/main.js
+++ pallet-picker/src/main.js
@@ -1,3 +1,87 @@
-import './style.css'
+import '../src/style/style.css'
+import '../src/style/form.css'
+import {v4 as uuid} from "uuid"
+// import defaultPallet from "./default-pallet.json"
+import {setPallets, getPallets, removePallet, addPallets, initializePallets} from "./function_components/local_storage"
+// setPallets(defaultPallet)
+const handleSubmit = (event) => {
+    event.preventDefault()
+    const form = event.target
+    const formData = new FormData(form)
+    console.log(formData)
+    const formObj = {
+        colors: formData.getAll("colors"),
+        title: formData.get('title'),
+        temperature: formData.get('temperature'),
+        uuid: uuid(),
+    }
 
-console.log("Hello, world")
+    
+    if (!(formObj.temperature) || !(formObj.title)) {
+        console.log(formObj)
+        return 
+    }
+
+    console.log(formObj.temperature)
+
+    const ul = document.querySelector('ul')
+
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild)
+    }
+
+    addPallets(formObj)
+    renderPallets()
+
+    form.reset()
+}
+
+const renderPallets = () => {
+    const ul = document.querySelector('ul')
+    const data = getPallets()
+    console.log(data)
+    data.forEach((input) => {
+        const li = document.createElement('li')
+        const h3 = document.createElement('h3')
+        const span1 = document.createElement('span')
+        const span2 = document.createElement('span')
+        const span3 = document.createElement('span')
+        const button = document.createElement('button')
+        const p = document.createElement('p')
+
+        h3.innerText = input.title
+        span1.innerText = input.colors[0]
+        span1.setAttribute("class", "paint")
+        span1.setAttribute("style", `background-color: ${input.colors[0]}`)
+        span2.setAttribute("class", "paint")
+        span2.setAttribute("style", `background-color: ${input.colors[1]}`)
+        span3.setAttribute("class", "paint")
+        span3.setAttribute("style", `background-color: ${input.colors[2]}`)
+        button.innerText = "Delete pallet"
+        button.addEventListener("click", () => {
+            ul.removeChild(li)
+            removePallet(input)
+        })
+        span2.innerText = input.colors[1]
+        span3.innerText = input.colors[2]
+        p.innerText = input.temperature
+        li.appendChild(h3)
+        li.appendChild(span1)
+        li.appendChild(span2)
+        li.appendChild(span3)
+        li.appendChild(button) 
+        li.appendChild(p)
+        ul.append(li)
+    })
+
+
+}
+
+const main = () => {
+    const form = document.querySelector('#palletForm')
+    form.addEventListener('submit', handleSubmit)
+    if (!getPallets()) initializePallets();
+    renderPallets()
+}
+
+main()
